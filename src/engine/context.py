d34df48fb1886d60b085ca88192501dc1a46ffd8
Rade Pejanović <radepejanovic@gmail.com>
2026-04-05 22:19:07 +0200

    engine & backend implementation

---
+++ src/engine/context.py
@@ -0,0 +1,11 @@
+class Context:
+    def __init__(self):
+        self._resources = {}
+
+    def store(self, name, resource):
+        self._resources[name] = resource
+
+    def get(self, name):
+        if name not in self._resources:
+            raise RuntimeError(f"Resource '{name}' not loaded")
+        return self._resources[name]
